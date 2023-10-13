7480c847b240601f6f79cfcd0aa9c36ab9f605cf
zakariarafi <111441196+zakariarafi@users.noreply.github.com>
2023-10-13 16:02:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Proyek Analisis Data Penyewaan Sepeda
+
+## Informasi Umum
+
+**Nama**: Zakaria Rafi  
+**Email**: zakariarafifahmi@gmail.com  
+**Id Dicoding**: zakaria_rafit
+
+## Deskripsi Projek
+
+Projek ini berfokus pada analisis data penyewaan sepeda untuk mendapatkan wawasan mengenai pola dan tren penyewaan sepeda. Dua dataset utama yang digunakan adalah `day.csv` dan `hour.csv`, yang berisi data harian dan jam penyewaan sepeda.
+
+## Pertanyaan Bisnis
+
+1. Bagaimana pola penyewaan sepeda harian berubah sepanjang tahun, dan apakah ada pola musiman?
+2. Pada jam berapa dalam sehari jumlah penyewaan sepeda mencapai puncaknya?
+
+## Analisis Data
+
+Analisis data dilakukan dengan menggunakan Python dan berbagai library seperti pandas, matplotlib, dan seaborn. Proses analisis meliputi eksplorasi data, visualisasi, dan interpretasi hasil untuk menjawab pertanyaan bisnis.
+
+### Teknik Analisis Lanjutan
+
+Teknik clustering digunakan untuk lebih memahami pola penyewaan sepeda dan mengelompokkan data berdasarkan karakteristik tertentu.
+
+## Dashboard
+
+Dashboard interaktif dibuat dengan menggunakan Streamlit untuk memvisualisasikan hasil analisis data secara interaktif. Dashboard dapat diakses [disini]
+
+## Cara Menjalankan Projek
+
+1. Clone repository ini.
+2. Install dependencies menggunakan
+3. Jalankan aplikasi streamlit dengan `streamlit run streamlit_app.py`.
---
+++ streamlit_app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates 
+import seaborn as sns
+import numpy as np  # untuk clustering
+
+# Load data
+day_df = pd.read_csv('day.csv')
+hour_df = pd.read_csv('hour.csv')
+
+# Informasi Proyek
+st.title('Proyek Analisis Data: Bike Sharing Data Analysis')
+
+st.markdown("""
+- **Nama:** Zakaria Rafi
+- **Email:** zakariarafifahmi@gmail.com
+- **ID Dicoding:** zakaria_rafi
+""")
+
+st.subheader('Menentukan Pertanyaan Bisnis')
+
+st.write("""
+- **Pertanyaan 1:** Bagaimana pola penyewaan sepeda harian berubah sepanjang tahun, dan apakah ada pola musiman?
+- **Pertanyaan 2:** Pada jam berapa dalam sehari jumlah penyewaan sepeda mencapai puncaknya?
+""")
+
+# Option to select data view
+option = st.selectbox('Select Data', ('Day Data', 'Hour Data'))
+if option == 'Day Data':
+    st.write(day_df)
+elif option == 'Hour Data':
+    st.write(hour_df)
+
+# ... (kode sebelumnya)
+
+# Menambahkan kesimpulan pertanyaan 1
+st.subheader('Kesimpulan Pertanyaan 1:')
+st.write("""
+Ada pola musiman dalam penyewaan sepeda, dengan peningkatan penyewaan sekitar April hingga September (bulan-bulan hangat).
+Tren penyewaan menunjukkan peningkatan dari tahun ke tahun.
+""")
+
+# Plotting+pallette
+plt.figure(figsize=(10, 5)) 
+sns.lineplot(data=day_df, x='dteday', y='cnt', hue='yr', palette="viridis")
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # format tanggal
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=-1, interval=3))  # label untuk tiap 3 bulan
+plt.xticks(rotation=45) #readability
+plt.title("Tren Penyewaan Sepeda Harian Sepanjang Tahun")
+plt.xlabel("Tanggal")
+plt.ylabel("Jumlah Penyewaan")
+st.pyplot(plt)
+
+# Menambahkan kesimpulan pertanyaan 2
+st.subheader('Kesimpulan Pertanyaan 2:')
+st.write("""
+Ada dua puncak penyewaan sepeda dalam sehari, yang berkorelasi dengan jam berangkat dan pulang kerja.
+Optimalisasi ketersediaan sepeda pada jam-jam sibuk dapat meningkatkan layanan kepada pelanggan.
+""")
+
+# Plotting distribusi penyewaan sepeda per jam+pallette
+plt.figure(figsize=(10, 5)) 
+sns.boxplot(data=hour_df, x='hr', y='cnt', palette="viridis")
+plt.title('Distribusi Penyewaan Sepeda per Jam')
+plt.xlabel('Jam')
+plt.ylabel('Jumlah Penyewaan')
+st.pyplot(plt)
+
+# Menambahkan Teknik Analisis Lanjutan
+st.subheader('Teknik Analisis Lanjutan: Clustering')
+st.write("""
+Setelah menganalisis dan mendapatkan kesimpulan mengenai data penyewaan sepeda, kita akan menerapkan teknik clustering untuk lebih memahami pola penyewaan sepeda dan mengelompokkan data berdasarkan karakteristik tertentu.
+""")
+
+# Mengkategorikan jam berdasarkan jumlah penyewaan
+hour_df['rental_category'] = pd.cut(hour_df['cnt'], bins=[0, 50, 100, np.inf], labels=['Low', 'Medium', 'High'])
+
+# Mengkategorikan hari berdasarkan jumlah penyewaan
+day_df['rental_category'] = pd.cut(day_df['cnt'], bins=[0, 1000, 2000, np.inf], labels=['Low', 'Medium', 'High'])
+
+# Menampilkan hasil kategorisasi
+print(hour_df[['hr', 'cnt', 'rental_category']].head())
+print(day_df[['dteday', 'cnt', 'rental_category']].head())
+
+# Plotting the distribution of rental categories throughout the day
+sns.countplot(data=hour_df, x='hr', hue='rental_category', palette='viridis')
+plt.title('Distribusi Kategori Penyewaan Sepanjang Hari.')
+plt.xlabel('Hour of the Day')
+plt.ylabel('Count')
+st.pyplot(plt)
