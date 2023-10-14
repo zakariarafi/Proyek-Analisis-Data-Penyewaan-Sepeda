e2b6153f6a590fedbec6f495db2d9a3e126aca57
zakariarafi <111441196+zakariarafi@users.noreply.github.com>
2023-10-14 13:24:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,79 +5,31 @@ import matplotlib.dates as mdates
 import seaborn as sns
 import numpy as np  # untuk clustering
 
-# Informasi Proyek
-st.title('Proyek Analisis Data: Bike Sharing Data Analysis')
-
-'''
 # Load data
 day_df = pd.read_csv('day.csv')
 hour_df = pd.read_csv('hour.csv')
 
-st.markdown("""
-- **Nama:** Zakaria Rafi
-- **Email:** zakariarafifahmi@gmail.com
-- **ID Dicoding:** zakaria_rafi
-""")
+# Informasi Proyek
+st.title('Proyek Analisis Data: Bike Sharing Data Analysis')
 
-st.subheader('Menentukan Pertanyaan Bisnis')
-
-st.write("""
-- **Pertanyaan 1:** Bagaimana pola penyewaan sepeda harian berubah sepanjang tahun, dan apakah ada pola musiman?
-- **Pertanyaan 2:** Pada jam berapa dalam sehari jumlah penyewaan sepeda mencapai puncaknya?
-""")
-
-# Option to select data view
-option = st.selectbox('Select Data', ('Day Data', 'Hour Data'))
-if option == 'Day Data':
-    st.write(day_df)
-elif option == 'Hour Data':
-    st.write(hour_df)
-
-
-# Menambahkan kesimpulan pertanyaan 1
-st.subheader('Kesimpulan Pertanyaan 1:')
-st.write("""
-Ada pola musiman dalam penyewaan sepeda, dengan peningkatan penyewaan sekitar April hingga September (bulan-bulan hangat).
-Tren penyewaan menunjukkan peningkatan dari tahun ke tahun.
-""")
-
-'''
-
-# Plotting+pallette
+# Plotting
 plt.figure(figsize=(10, 5)) 
 sns.lineplot(data=day_df, x='dteday', y='cnt', hue='yr', palette="viridis")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))  # format tanggal
 plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=-1, interval=3))  # label untuk tiap 3 bulan
-plt.xticks(rotation=45) #readability
+plt.xticks(rotation=45)  # readability
 plt.title("Tren Penyewaan Sepeda Harian Sepanjang Tahun")
 plt.xlabel("Tanggal")
 plt.ylabel("Jumlah Penyewaan")
 st.pyplot(plt)
 
-'''
-# Menambahkan kesimpulan pertanyaan 2
-st.subheader('Kesimpulan Pertanyaan 2:')
-st.write("""
-Ada dua puncak penyewaan sepeda dalam sehari, yang berkorelasi dengan jam berangkat dan pulang kerja.
-Optimalisasi ketersediaan sepeda pada jam-jam sibuk dapat meningkatkan layanan kepada pelanggan.
-""")
-'''
-
-# Plotting distribusi penyewaan sepeda per jam+pallette
+# Plotting distribusi penyewaan sepeda per jam
 plt.figure(figsize=(10, 5)) 
 sns.boxplot(data=hour_df, x='hr', y='cnt', palette="viridis")
 plt.title('Distribusi Penyewaan Sepeda per Jam')
 plt.xlabel('Jam')
 plt.ylabel('Jumlah Penyewaan')
 st.pyplot(plt)
-
-'''
-# Menambahkan Teknik Analisis Lanjutan
-st.subheader('Teknik Analisis Lanjutan: Clustering')
-st.write("""
-Setelah menganalisis dan mendapatkan kesimpulan mengenai data penyewaan sepeda, kita akan menerapkan teknik clustering untuk lebih memahami pola penyewaan sepeda dan mengelompokkan data berdasarkan karakteristik tertentu.
-""")
-'''
 
 # Mengkategorikan jam berdasarkan jumlah penyewaan
 hour_df['rental_category'] = pd.cut(hour_df['cnt'], bins=[0, 50, 100, np.inf], labels=['Low', 'Medium', 'High'])
